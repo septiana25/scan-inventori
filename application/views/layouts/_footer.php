c5ba4248def904f71ae0963f29b0b3aacb75becc
Ian Septiana <insaidseptiana@gmail.com>
2024-01-08 09:51:15 +0700

    fix menu button

--- application/views/layouts/_footer.php
+++ application/views/layouts/_footer.php
@@ -3,6 +3,14 @@
     <nav class="container">
         <div class="row row-cols-4">
             <div class="col btnMenu">
+                <button onclick="window.location.href='<?= base_url(); ?>'">
+                    <div class="btnIcon">
+                        <i class="fa fa-home" aria-hidden="true"></i>
+                    </div>
+                    <p>Home</p>
+                </button>
+            </div>
+            <div class="col btnMenu">
                 <button onclick="window.location.href='<?= base_url('masuk/index'); ?>'">
                     <div class="btnIcon">
                         <i class="fa fa-reply" aria-hidden="true"></i>
@@ -17,15 +25,6 @@
                         <i class="fa fa-share" aria-hidden="true"></i>
                     </div>
                     <p>Keluar</p>
-                </button>
-
-            </div>
-            <div class="col btnMenu">
-                <button onclick="window.location.href='#'">
-                    <div class="btnIcon">
-                        <i class="fa fa-book" aria-hidden="true"></i>
-                    </div>
-                    <p>Laporan</p>
                 </button>
 
             </div>
